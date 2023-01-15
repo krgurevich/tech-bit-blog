5095be2db8d01bf3ed6e76e3ed4ef013b5cfe199
krgurevich <kananyan@optonline.net>
2023-01-15 09:31:47 -0500

    updated the signup functionality code

--- public/js/login.js
+++ public/js/login.js
@@ -1,27 +1,27 @@
 const loginFormHandler = async (event) => {
-    // Prevents the browser from submitting the form
-    event.preventDefault();
-  
-    // Gather the data from the form elements on the page
-    const username = document.querySelector('#username').value.trim();
-    const password = document.querySelector('#password').value.trim();
-  
-    if (username && password) {
-      // Send the e-mail and password to the server
-      const response = await fetch('/api/users/login', {
-        method: 'POST',
-        body: JSON.stringify({ username, password }),
-        headers: { 'Content-Type': 'application/json' },
-      });
-  
-      if (response.ok) {
-        document.location.replace('/');
-      } else {
-        alert('Please try again. Incorrect username or password.');
-      }
+  // Prevents the browser from submitting the form
+  event.preventDefault();
+
+  // Gather the data from the form elements on the page
+  const username = document.querySelector("#username").value.trim();
+  const password = document.querySelector("#password").value.trim();
+
+  if (username && password) {
+    // Send the username and password to the server
+    const response = await fetch("/api/users/login", {
+      method: "POST",
+      body: JSON.stringify({ username, password }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (response.ok) {
+      document.location.replace("/");
+    } else {
+      alert("Please try again. Incorrect username or password.");
     }
-  };
-  
-  document
-    .querySelector('.login-container')
-    .addEventListener('submit', loginFormHandler);
+  }
+};
+
+document
+  .querySelector(".login-container")
+  .addEventListener("submit", loginFormHandler);
--- public/js/signup.js
+++ public/js/signup.js
@@ -1,24 +1,24 @@
 const signupFormHandler = async (event) => {
-    event.preventDefault();
-  
-    const username = document.querySelector('#signup-username').value.trim();
-    const password = document.querySelector('#signup-password').value.trim();
-  
-    if (username && password) {
-      const response = await fetch('/api/users', {
-        method: 'POST',
-        body: JSON.stringify({ username, password }),
-        headers: { 'Content-Type': 'application/json' },
-      });
-  
-      if (response.ok) {
-        document.location.replace('/dashboard');
-      } else {
-        alert(response.statusText);
-      }
+  event.preventDefault();
+
+  const username = document.querySelector("#signup-username").value.trim();
+  const password = document.querySelector("#signup-password").value.trim();
+
+  if (username && password) {
+    const response = await fetch("/api/users", {
+      method: "POST",
+      body: JSON.stringify({ username, password }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (response.ok) {
+      document.location.replace("/");
+    } else {
+      alert("Please try again!");
     }
-  };
-  
-  document
-  .querySelector('.signup-container')
-  .addEventListener('submit', signupFormHandler);
+  }
+};
+
+document
+  .querySelector(".signup-container")
+  .addEventListener("submit", signupFormHandler);
